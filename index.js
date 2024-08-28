976f4842f3321ff843d9fd7715b05fe0105a90d0
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-08-28 20:22:29 +0545

    add multer file storage

--- index.js
+++ index.js
@@ -2,14 +2,32 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = 4000;
+const multer = require("multer");
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   return res.render("home");
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("fileuploads"), (req, res) => {
+  console.log(req.file, req.body);
+
+  return res.redirect("/");
 });
 
 app.listen(PORT, () => {
